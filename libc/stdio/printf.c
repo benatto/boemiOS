65262d765d375dfca17a76e976b472bf8eae83f1
Marco A Benatto <marco.antonio.780@gmail.com>
2016-08-08 15:40:49 -0400

    libc/stdio: Adding support for int on printf
    Adding support for int (both %d and %x) on printf function
    
    Signed-off-by: Marco A Benatto <marco.antonio.780@gmail.com>

--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -2,7 +2,44 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
- 
+
+static void itoa(char *buf, int base, int d) {
+	char *p = buf;
+    char *p1, *p2;
+    unsigned long ud = d;
+    int divisor = 10;
+
+    /* If %d is specified and D is minus, put `-' in the head. */
+    if (base == 'd' && d < 0) {
+        *p++ = '-';
+        buf++;
+        ud = -d;
+	} else if (base == 'x') {
+      divisor = 16;
+	}
+
+    /* Divide UD by DIVISOR until UD == 0. */
+    do {
+        int remainder = ud % divisor;
+        *p++ = (remainder < 10) ? remainder + '0' : remainder + 'a' - 10;
+	}
+    while (ud /= divisor);
+
+    /* Terminate BUF. */
+    *p = 0;
+
+    /* Reverse BUF. */
+    p1 = buf;
+    p2 = p - 1;
+    while (p1 < p2) {
+        char tmp = *p1;
+        *p1 = *p2;
+        *p2 = tmp;
+        p1++;
+        p2--;
+	}
+}
+
 static void print(const char *data, size_t data_length) {
 	for ( size_t i = 0; i < data_length; i++ )
 		putchar((int) ((const unsigned char*) data)[i]);
@@ -16,8 +53,8 @@ int printf(const char *restrict format, ...) {
 	size_t amount;
 	bool rejected_bad_specifier = false;
  
-	while ( *format != '\0' ) {
-		if ( *format != '%' ) {
+	while (*format != '\0') {
+		if (*format != '%') {
 		print_c:
 			amount = 1;
 			while ( format[amount] && format[amount] != '%' )
@@ -30,25 +67,29 @@ int printf(const char *restrict format, ...) {
  
 		const char *format_begun_at = format;
  
-		if ( *(++format) == '%' )
+		if (*(++format) == '%')
 			goto print_c;
  
-		if ( rejected_bad_specifier ) {
+		if (rejected_bad_specifier) {
 		incomprehensible_conversion:
 			rejected_bad_specifier = true;
 			format = format_begun_at;
 			goto print_c;
 		}
  
-		if ( *format == 'c' ) {
+		if (*format == 'c') {
 			format++;
 			char c = (char) va_arg(parameters, int /* char promotes to int */);
 			print(&c, sizeof(c));
-		}
-		else if ( *format == 's' ) {
+		} else if (*format == 's') {
 			format++;
 			const char *s = va_arg(parameters, const char*);
 			print(s, strlen(s));
+		} else if (*format == 'd' || *format == 'x') {
+			/*TODO: When we have malloc use it here*/
+			char buf[16];
+			itoa(buf, *format, va_arg(parameters, int));
+			printf(buf, strlen(buf));
 		} else {
 			goto incomprehensible_conversion;
 		}
